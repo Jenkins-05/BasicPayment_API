2ddb16cab59cf493b9dae726fd111dfb577d6fde
kvbo <kayongokayhood@gmail.com>
2024-04-06 10:07:39 +0200

    first commit

---
+++ payments/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from users.models import Account
+
+
+# Create your models here.
+class Transaction(models.Model):
+    STATUS_CHOICES = (
+        ('SUCCEESS', 'success'),
+        ('DECLINED', 'declined'),
+    )
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    users = models.ForeignKey(Account, related_name="transactions", on_delete=models.CASCADE)
+
+class PaymentMethod(models.Model):
+    PAYMENT_METHOD = (
+            ('MTN', 'MTN'),
+            ('AIRTEL', 'AIRTEL')
+    )
+    
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    transactions = models.OneToOneField(Transaction, related_name="payment_method", on_delete=models.CASCADE )
